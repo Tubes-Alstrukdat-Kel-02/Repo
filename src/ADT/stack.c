f3875aa88b1051f6f7fcc8bc16db83472e328246
kachuuuwww <wfksamsea@gmail.com>
2021-11-22 12:52:52 +0700

    adding adt stack

--- src/ADT/stack.c
+++ src/ADT/stack.c
@@ -1,47 +1,39 @@
-#include "stack.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "stack.h"
 
-void CreateEmpty (Stack *S){
-    Top(*S) = Nil;
-}
+/* *** Konstruktor/Kreator *** */
+void CreateEmptyStack (Stack *S){
 /* I.S. sembarang; */
 /* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
 /* jadi indeksnya antara 1.. MaxEl+1 karena 0 tidak dipakai */
 /* Ciri stack kosong : TOP bernilai Nil */
+    Top(*S) = Nil;
+}
 
 /* ************ Predikat Untuk test keadaan KOLEKSI ************ */
-boolean IsEmpty (Stack S){
-    return Top(S) == Nil;
-}
+boolean IsEmptyStack (Stack S){
 /* Mengirim true jika Stack kosong: lihat definisi di atas */
-boolean IsFull (Stack S){
-    return Top(S) == MaxEl;
+    return (Top(S) == Nil);
 }
+boolean IsFullStack (Stack S){
 /* Mengirim true jika tabel penampung nilai elemen stack penuh */
+    return (Top(S) == MaxEl);
+}
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push (Stack * S, infotype X){
-    if (!IsEmpty(*S))
-    {
-        Top(*S)++;
-    }
-    else
-    {
-        Top(*S) = 1;
-    }
-    InfoTop(*S) = X;
-}
+void PushStack (Stack * S, infotype X){
 /* Menambahkan X sebagai elemen Stack S. */
 /* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+    Top(*S)++;
+    InfoTop(*S) = X;
+}
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void Pop (Stack * S, infotype* X){
-    *X = InfoTop(*S);
-    Top(*S)--;
-}
+void PopStack (Stack * S, infotype* X){
 /* Menghapus X dari Stack S. */
 /* I.S. S  tidak mungkin kosong */
 /* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
-
+    *X = InfoTop(*S);
+    Top(*S)--;
+}
