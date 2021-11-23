3efed455138b014beb894ac6761247c25690412d
yahyaqr <aqrom.yahya75@gmail.com>
2021-11-23 10:04:50 +0700

    Revert "Revert "Merge pull request #4 from yahyaqr/main""
    This reverts commit a58757a832c094dfb4c31c02f39a10cad1cdef36.

--- src/Func/gameplay.c
+++ src/Func/gameplay.c
@@ -34,6 +34,8 @@ void roundLoop() {
     }
 
     turnLoop();
+
+    // 15 disini jangan lupa ganti sama variable finish line
   } while (playerLocation[playerTurn] != 15);
 }
 
@@ -66,12 +68,11 @@ void inputCommand() {
 }
 
 void commandSwitchCase() {
-  printf("\n");
-  printf("Masukkan command: ");
+  printf("\nMasukkan command: ");
   scanf("%d", &command);
   while (command < 1 || command > 8) {
-    printf("Input tidak valid, harap masukkan bilangan 1.\n");
-    printf("Masukkan command: ");
+    printf("Input tidak valid, harap masukkan bilangan 1-8.\n");
+    printf("\nMasukkan command: ");
     scanf("%d", &command);
   }
   switch (command){
